8a7e97c4fdc43bad11824e62ea0f295267675ee1
Cole Chamberlain <cole.chamberlain@gmail.com>
2015-06-06 23:38:27 -0700

    heavy refactoring to configs and tig-link

--- etc/paths.js
+++ etc/paths.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var join = require('path').join;
-
-exports.TIG_ROOT=process.env.TIG_HOME || join(process.env.HOME || process.env.USERPROFILE, ".tig");
+var TIG_ROOT = process.env.TIG_HOME || join(process.env.HOME || process.env.USERPROFILE, ".tig");
+exports.TIG_ROOT=TIG_ROOT;
 exports.TIGRC_PATH=join(TIG_ROOT, "tigrc");
 exports.TIG_JSON_PATH=join(TIG_ROOT, "tig.json");
 exports.TIG_LINK_JSON_PATH=join(TIG_ROOT, "tig-link.json");
-
